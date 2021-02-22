4c8c8fe16a392200044e15f948413b98efe0fd47
therealshizan <shaikhshizan9833@gmail.com>
2021-02-22 21:25:03 +0530

    BG Gradient Checker

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let body = document.getElementsByTagName("BODY")[0];
+let firstHex = document.querySelector("#first_hex");
+let secondHex = document.querySelector("#second_hex");
+let changeBg = document.querySelector("#change_bg");
+let content = document.querySelector(".content");
+
+changeBg.addEventListener("click", colorChange)
+
+function colorChange(){
+  body.style.background = 'linear-gradient(to right, '
+        + firstHex.value + ', ' + secondHex.value + ')';
+  content.style.color = "#fff"; 
+  changeBg.style.border = "1px solid #fff";
+  changeBg.style.color = "#fff";
+}
