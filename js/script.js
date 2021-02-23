8622ac37d1b24ecf2168ce12213b0a3fc000e22f
Shizan Shaikh <51877872+therealshizan@users.noreply.github.com>
2021-02-23 12:54:14 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 let body = document.getElementsByTagName("BODY")[0];
+let gradientDirection = document.querySelector("#gradient_direction");
 let firstHex = document.querySelector("#first_hex");
 let secondHex = document.querySelector("#second_hex");
 let changeBg = document.querySelector("#change_bg");
@@ -7,8 +8,9 @@ let content = document.querySelector(".content");
 changeBg.addEventListener("click", colorChange)
 
 function colorChange(){
-  body.style.background = 'linear-gradient(to right, '
-        + firstHex.value + ', ' + secondHex.value + ')';
+  // body.style.background = 'linear-gradient(to right, '
+  //       + firstHex.value + ', ' + secondHex.value + ')';
+  body.style.background = `linear-gradient(to ${gradientDirection.value} , ${firstHex.value} , ${secondHex.value})`;
   content.style.color = "#fff"; 
   changeBg.style.border = "1px solid #fff";
   changeBg.style.color = "#fff";
